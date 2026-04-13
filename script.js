1c22f2517f3604b07bf09700a292952396519616
Taro <rout0052@algonquinlive.com>
2026-04-13 14:38:41 -0400

    Add reset btn

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const userImg = document.getElementById("user-pic");
 const scaleRange = document.getElementById("imgScale");
+const resetBtn = document.getElementById("reset");
+const display = document.getElementById("display");
 
-const loadFile = function(event) {
-    userImg.src=URL.createObjectURL(event.target.files[0]);
-};
 
 let imgX = 0;
 let imgY = 0;
+
+const loadFile = function(event) {
+    userImg.src=URL.createObjectURL(event.target.files[0]);
+    scaleRange.value = 100;
+};
 
 const up = function() {
     imgY -= 5;
@@ -28,11 +32,19 @@ const right = function() {
     userImg.style.transform = `translate(${imgX}px, ${imgY}px)`;
 };
 
-
-const updateImgSize = function() {
+const scaleImg = function() {
     // userImg.setAttribute('height', `calc(75% * ${scaleRange.value})`);
     userImg.style.scale = scaleRange.value / 100;
 }
+
+function reset() {
+    userImg.style.transform = "";
+    userImg.style.scale = "";
+    scaleRange.value = 100;
+}
+
+resetBtn.addEventListener("click", reset);
+
 
 
 
